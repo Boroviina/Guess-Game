63421d0161d9c83d3ef539dee353adc49e6cec60
Boroviina@protonmail.com <117476003+Boroviina@users.noreply.github.com>
2024-04-19 21:16:33 +0200

    GAmeOverScreen landscape ui corrections

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,13 +1,27 @@
-import {Image, Text, View, StyleSheet, Dimensions} from "react-native";
+import {Image, Text, View, StyleSheet, useWindowDimensions} from "react-native";
 import Title from "../components/title";
 import Colors from "../constans/colors";
 import PrimaryButton from "../components/PrimaryButton";
 
 function GameOverScreen(props) {
+
+    const {width, height} = useWindowDimensions();
+    let imageSize = 300;
+    if (width < 380) {
+        imageSize = 150;
+    }
+    if (height < 400) {
+        imageSize = 80;
+    }
+    const imageStyle = {
+        width: imageSize,
+        height: imageSize,
+        borderRadius: imageSize / 2
+    }
     return (
         <View style={styles.screenContainer}>
             <Title>GAME OVER!</Title>
-            <View style={styles.imageContainer}>
+            <View style={[styles.imageContainer, imageStyle ]}>
                 <Image style={styles.image} source={require('../assets/images/success.png')}/>
             </View>
             <View style={styles.newGameContainer}>
@@ -23,13 +37,13 @@ function GameOverScreen(props) {
 
 export default GameOverScreen;
 
-const deviceWidth = Dimensions.get('window').width;
+// const deviceWidth = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
     imageContainer: {
-        width: deviceWidth < 380 ? 150 : 300,
-        height: deviceWidth < 380 ? 150 : 300,
-        borderRadius: deviceWidth < 380 ? 75 : 150,
+        // width: deviceWidth < 380 ? 150 : 300,
+        // height: deviceWidth < 380 ? 150 : 300,
+        // borderRadius: deviceWidth < 380 ? 75 : 150,
         borderWidth: 3,
         borderColor: Colors.primary900,
         overflow: "hidden",
